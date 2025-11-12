7b89fa685aca2b46111c9010cfe81088ba4cdad8
EDUARDO <s24021887@estudiantes.uv.mx>
2025-11-12 16:20:39 -0600

    update personal v.3 We add Coordinadora class

--- src/Package/Coordinadora.java
+++ src/Package/Coordinadora.java
@@ -1,13 +1,17 @@
 package Package;
+import java.sql.Date;
 
-public class Coordinadora {
+public class Coordinadora extends Personal{
     private String cursos;
 
-    public Coordinadora(String cursos){
+    public Coordinadora(String nombre, Date fechaNacimiento, String clavePersonal, String passPersonal,String rolId, String cursos) {
+        super(nombre, fechaNacimiento, clavePersonal, passPersonal, rolId);
         this.cursos = cursos;
     }
     public String getCursos() {
         return cursos;
     }
-    public void setCursos(String cursos) {}
+    public void setCursos(String cursos) {
+        this.cursos = cursos;
+    }
 }
