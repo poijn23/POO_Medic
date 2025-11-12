3a4f67b48f92e9a4ddb33c6005579cad8c22b925
EDUARDO <s24021887@estudiantes.uv.mx>
2025-11-12 16:13:33 -0600

    update personal v.2

---
+++ src/Package/Coordinadora.java
@@ -0,0 +1,13 @@
+package Package;
+
+public class Coordinadora {
+    private String cursos;
+
+    public Coordinadora(String cursos){
+        this.cursos = cursos;
+    }
+    public String getCursos() {
+        return cursos;
+    }
+    public void setCursos(String cursos) {}
+}
