7e94017662ab52fdfd2c45a250bbaaabd57c8755
CesarAMG47 <zs24013291@estudiantes.uv.mx>
2025-11-12 16:13:42 -0600

    Clase Estudiante

--- src/Package/Estudiante.java
+++ src/Package/Estudiante.java
@@ -27,11 +27,30 @@ public class Estudiante {
         this.hojaPresentacion = hojaPresentacion;
     }
     // Getters
-
-
     public String getNombreEst() {return nombreEst;}
     public int getMatricula() {return matricula;}
     public String getContrasena() {return contrasena;}
+    public String getArea() {return area;}
+    public String getGuardia() {return guardia;}
+    public String getTipoEstudiante() {return tipoEstudiante;}
+    public String getINE() {return INE;}
+    public String getCURP() {return CURP;}
+    public String getActaNacimiento() {return actaNacimiento;}
+    public String getHojaPresentacion() {return hojaPresentacion;}
+
+    // Setters
+    public void setNombreEst(String nombreEst) {this.nombreEst = nombreEst;}
+    public void setMatricula(int matricula) {this.matricula = matricula;}
+    public void setContrasena(String contrasena) {this.contrasena = contrasena;}
+    public void setArea(String area) {this.area = area;}
+    public void setGuardia(String guardia) {this.guardia = guardia;}
+    public void setTipoEstudiante(String tipoEstudiante) {this.tipoEstudiante = tipoEstudiante;}
+    public void setINE(String INE) {this.INE = INE;}
+    public void setCURP(String CURP) {this.CURP = CURP;}
+    public void setActaNacimiento(String actaNacimiento) {this.actaNacimiento = actaNacimiento;}
+    public void setHojaPresentacion(String hojaPresentacion) {this.hojaPresentacion = hojaPresentacion;}
+
+
 }
 
 
