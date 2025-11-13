fed5b91542c18882521b03145e336a5b5d3e5805
poijn <josepriorhernandez@gmail.com>
2025-11-13 17:37:56 -0600

    Interface Asistencia simplificado

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,10 @@
 
-import Package.RegistroAsistencia_metodos;
 import Package.RegistroAsistencia;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
 public class Main {
     private static RegistroAsistencia registroAsistenciaPanel;
-    private static RegistroAsistencia_metodos  registroAsistenciaMetodos;
     private static JFrame frame;
     public static void main(String[] args) {
     SwingUtilities.invokeLater(Main::createAndShowGUI);
@@ -14,9 +12,6 @@ public class Main {
 
     private static void createAndShowGUI() {
         registroAsistenciaPanel = new RegistroAsistencia();
-        registroAsistenciaMetodos = new RegistroAsistencia_metodos(registroAsistenciaPanel);
-        registroAsistenciaPanel.confirmarButton.addActionListener(registroAsistenciaMetodos);
-        registroAsistenciaPanel.confirmarButton.setActionCommand("Confirmar");
         frame=new JFrame("Registro Asistencia");
         frame.setContentPane(registroAsistenciaPanel.panel1);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
--- src/Package/RegistroAsistencia.java
+++ src/Package/RegistroAsistencia.java
@@ -1,19 +1,25 @@
 package Package;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class RegistroAsistencia {
+public class RegistroAsistencia implements ActionListener {
     public JPanel panel1;
     private JTextField Matricula;
     private JPasswordField password;
     public JButton confirmarButton;
 
-        public JTextField getMatricula() {
-            return Matricula;
+    public RegistroAsistencia() {
+        confirmarButton.addActionListener(this);
+        confirmarButton.setActionCommand("Confirmar");
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getActionCommand().equals("Confirmar")) {
+            System.out.println(this.Matricula.getText());
+            System.out.println(new String(password.getPassword()));
         }
-
-        public JPasswordField getPassword() {
-            return password;
-        }
+    }
 }
 
