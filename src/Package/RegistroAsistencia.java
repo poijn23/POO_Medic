ef1a838bc61ae81670917fbbb6d97df627a34590
poijn <josepriorhernandez@gmail.com>
2025-11-13 15:04:58 -0600

    Interface Asistencia

---
+++ src/Package/RegistroAsistencia.java
@@ -0,0 +1,19 @@
+package Package;
+
+import javax.swing.*;
+
+public class RegistroAsistencia {
+    public JPanel panel1;
+    private JTextField Matricula;
+    private JPasswordField password;
+    public JButton confirmarButton;
+
+        public JTextField getMatricula() {
+            return Matricula;
+        }
+
+        public JPasswordField getPassword() {
+            return password;
+        }
+}
+
