316d81db81fca9e3d3efdd5c62b69eeb0e63bcc0
EDUARDO <s24021887@estudiantes.uv.mx>
2025-11-14 17:08:42 -0600

    update interfas de inicio de secionv.1

--- src/Package/InicioSesion_Personal.java
+++ src/Package/InicioSesion_Personal.java
@@ -1,5 +1,10 @@
 package Package;
+  import javax.swing.*;
 
 public class InicioSesion_Personal {
+private JPanel cocntentpane;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton iniciarButton;
 
 }
