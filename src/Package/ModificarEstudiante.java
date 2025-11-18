f2346dba72db5eea5eb286da2b585eb7db24b4b7
CesarAMG47 <zs24013291@estudiantes.uv.mx>
2025-11-18 01:02:12 -0600

    interfaz de Modificar Estudiante base

--- src/Package/ModificarEstudiante.java
+++ src/Package/ModificarEstudiante.java
@@ -5,8 +5,12 @@ public class ModificarEstudiante extends JFrame{
     private JPanel panelContenido;
     private JTextField campoNombre;
     private JTextField campoMatricula;
-    private JComboBox comboBox1;
-    private JCheckBox checkBox1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JTextField textField6;
     private JButton cancelarButton;
     private JButton modificarButton;
 
