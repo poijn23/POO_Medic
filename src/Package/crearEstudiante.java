af5baf2ac9a67cb8773b5b5012e8e821f897ef01
Luis Ángel Ortiz López <zs24013270@estudiantes.uv.mx>
2025-11-19 13:12:34 -0600

    CREAR estudiante

--- src/Package/crearEstudiante.java
+++ src/Package/crearEstudiante.java
@@ -6,7 +6,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 
-public class crearEstudiante {
+public class crearEstudiante{
 
     // --- VARIABLES ---
     private JPanel panelPrincipal;
