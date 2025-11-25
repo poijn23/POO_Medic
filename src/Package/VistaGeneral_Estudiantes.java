b22399205ea5e0d153e1b519f646b2aa0c48f3a2
RoazZz <goliatcalixto@gmail.com>
2025-11-24 18:18:51 -0600

    Actualización de la Clase VistaGeneral_Estudiantes

--- src/Package/VistaGeneral_Estudiantes.java
+++ src/Package/VistaGeneral_Estudiantes.java
@@ -1,8 +1,11 @@
 package Package;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class VistaGeneral_Estudiantes extends JFrame {
+    private final Consult_Database myDatabase = new Consult_Database();
     public JButton subirConstancia;
     public VistaGeneral_Estudiantes() {
         setTitle("VistaGeneral_Administración");
@@ -19,8 +22,23 @@ public class VistaGeneral_Estudiantes extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
     }
+
     public void inicializarComponentes(){
         subirConstancia = new JButton("Subir Constancia");
     }
 
+    public void actionPerformed(ActionEvent e) {
+        switch (e.getActionCommand()) {
+            case "Subir Constancia": {
+                this.dispose();
+                SubirConstancia constanciaVista = new SubirConstancia();
+                break;
+            }
+            default: {
+                break;
+            }
+        }
+    }
 }
+
+
