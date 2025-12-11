d5e5d2721d0e504e679a654428533bf371f2569d
ArturoBasa <zs24013319@estudiantes.uv.mx>
2025-12-10 22:11:39 -0600

    implemto de las clases: periodo, guardia y subir calificacion

---
+++ src/Package/Guardia.java
@@ -0,0 +1,33 @@
+package Package;
+
+public class Guardia {
+    private String tipoGuardia;
+    private String horario;
+    private String zonaTrabajo;
+
+    public Guardia(String tipoGuardia, String horario, String zonaTrabajo) {
+        this.tipoGuardia = tipoGuardia;
+        this.horario = horario;
+        this.zonaTrabajo = zonaTrabajo;
+    }
+
+    public String getTipoGuardia() {
+        return tipoGuardia;
+    }
+    public void setTipoGuardia(String tipoGuardia) {
+        this.tipoGuardia = tipoGuardia;
+    }
+    public String getHorario() {
+        return horario;
+    }
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+    public String getZonaTrabajo() {
+        return zonaTrabajo;
+    }
+    public void setZonaTrabajo(String zonaTrabajo) {
+        this.zonaTrabajo = zonaTrabajo;
+    }
+}
+
