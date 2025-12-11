4c69b29aa3dbd25b89327954628dcbfa8eeba86a
Luis Ángel Ortiz López <zs24013270@estudiantes.uv.mx>
2025-12-11 00:59:38 -0600

    .

--- src/Package/crearEstudiante.java
+++ src/Package/crearEstudiante.java
@@ -60,10 +60,6 @@ public class crearEstudiante extends JFrame {
         EspecialidadcomboBox.addItem("Odontologia");
         EspecialidadcomboBox.addItem("Enfermeria");
         EspecialidadcomboBox.addItem("Nutricion");
-        EspecialidadcomboBox.addItem("Medicina");
-        EspecialidadcomboBox.addItem("Odontologia");
-        EspecialidadcomboBox.addItem("Enfermeria");
-        EspecialidadcomboBox.addItem("Nutricion");
 
         //mostrar las opciones de medicina por defecto
         llenarComboTipoParaMedicina();
